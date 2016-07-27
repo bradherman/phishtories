c37d287346b0a02a77789e003e236c771a0ac0bd
Brad Herman <brad@hired.com>
2016-07-26 20:56:07 -0700

    add basic user specs

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :omniauthable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
+
+  validates :email, presence: true, email: true
 end
