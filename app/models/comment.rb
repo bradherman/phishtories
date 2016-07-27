7c6c4599c27dbcbbc16833271cc8df28bb28e847
Brad Herman <brad@hired.com>
2016-07-26 22:07:06 -0700

    add comment parent/child associations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,8 +2,9 @@ class Comment < ApplicationRecord
   validates :body, :story_id, :user_id, presence: true
   validates_associated :user, :story, :parent
 
-  has_one :parent, class_name: 'Comment'
+  has_many :children, class_name: 'Comment', foreign_key: 'parent_id'
 
   belongs_to :user
   belongs_to :story
+  belongs_to :parent, class_name: 'Comment', foreign_key: 'parent_id'
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -21,4 +21,11 @@ RSpec.describe Comment, type: :model do
     it { is_expected.to validate_presence_of(:story_id) }
     it { is_expected.to validate_presence_of(:user_id) }
   end
+
+  context 'associations' do
+    it { is_expected.to have_many(:children) }
+    it { is_expected.to belong_to(:parent) }
+    it { is_expected.to belong_to(:user) }
+    it { is_expected.to belong_to(:story) }
+  end
 end
