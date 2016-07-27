254d2721a7dd4e10e2f1db7c2890989028aac72a
Brad Herman <brad@hired.com>
2016-07-26 20:58:32 -0700

    add devise helpers to rspec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -56,6 +56,9 @@ RSpec.configure do |config|
   # config.filter_gems_from_backtrace("gem name")
 
   config.include AttributeNormalizer::RSpecMatcher, type: :model
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include Devise::Test::ControllerHelpers, type: :view
+  config.include Devise::Test::IntegrationHelpers, type: :feature
 end
 
 Shoulda::Matchers.configure do |config|
