a5dc5e0510f267f997da6baf1b3e14624ada5413
Brad Herman <brad@hired.com>
2016-07-26 18:54:18 -0700

    add basic story db specs

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 
 
 gem 'rails', '~> 5.0.0'
+# gem 'administrate', github: 'lyonsv/administrate', branch: 'rails-5'
 gem 'pg', '~> 0.18'
 gem 'puma', '~> 3.0'
 gem 'sass-rails', '~> 5.0'
--- spec/models/story_spec.rb
+++ spec/models/story_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Story, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to have_db_column(:body).of_type(:text) }
+  it { is_expected.to have_db_column(:title).of_type(:string) }
+  it { is_expected.to have_db_column(:author).of_type(:string) }
+  it { is_expected.to have_db_column(:author_location).of_type(:string) }
+  it { is_expected.to have_db_column(:author_email).of_type(:string) }
+  it { is_expected.to have_db_column(:venue).of_type(:string) }
+  it { is_expected.to have_db_column(:location).of_type(:string) }
+  it { is_expected.to have_db_column(:date).of_type(:date) }
+  it { is_expected.to have_db_column(:period).of_type(:integer) }
+  it { is_expected.to have_db_column(:votes).of_type(:integer).with_options(default: 0) }
+  it { is_expected.to have_db_column(:flagged).of_type(:boolean).with_options(default: false) }
+  it { is_expected.to have_db_column(:removed).of_type(:boolean).with_options(default: false) }
+  it { is_expected.to have_db_column(:created_at).of_type(:datetime) }
+  it { is_expected.to have_db_column(:updated_at).of_type(:datetime) }
 end
